6bb7d19d6ae5e59d4f8ff3e97094fa70e3986836
Elias P. <eliasasmark@hotmail.com>
2024-03-06 09:04:08 +0100

    initial commit

---
+++ src/Game/App.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.net.*;
+import java.io.*;
+import javafx.application.Application;;
+
+public class App {
+	public static Player me;
+	public static void main(String[] args) throws Exception{	
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Indtast spillernavn");
+		String navn = inFromUser.readLine();
+		me= GameLogic.makePlayer(navn);
+		GameLogic.makeVirtualPlayer(); // to be removed
+		Application.launch(Gui.class);
+	}
+}
+;
